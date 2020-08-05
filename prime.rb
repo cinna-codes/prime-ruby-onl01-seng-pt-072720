b335f075e14af1a26e4aec2153edf5f0cb1d2756
Cinna Goldman <somethingfellipickeditup@gmail.com>
2020-08-05 23:49:27 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -8,7 +8,15 @@ count = 0
   else num > 2
     i = 2
     while (i < num)
-
+      if (num % i == 0)
+        count += 1
+      end
+      i += 1
     end
+  end
+  if count > 1
+    return false
+  else
+    return true
   end
 end
