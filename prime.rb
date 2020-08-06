bb01c0487c212a8fc5102c4fec830ee40b3622d0
Cinna Goldman <somethingfellipickeditup@gmail.com>
2020-08-06 00:10:40 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,24 @@
 # Add  code here!
+def prime?(num)
+count = 0
+  if num < 2
+    return false
+  # elsif num == 2
+  #   return true
+  elsif num == 4
+    return false
+  else
+    i = 2
+    while (i < num)
+      if (num % i == 0)
+        count += 1
+      end
+      i += 1
+    end
+  end
+  if count > 1
+    return false
+  else
+    return true
+  end
+end
